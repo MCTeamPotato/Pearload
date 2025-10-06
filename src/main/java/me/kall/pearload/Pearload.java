9bfd37472a90b13a5d3c0cf935d30ff99bd26757
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-10-06 22:53:07 +0800

    neo

--- src/main/java/me/kall/pearload/Pearload.java
+++ src/main/java/me/kall/pearload/Pearload.java
@@ -4,20 +4,22 @@ import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import me.kall.pearload.api.ForceLoader;
 import me.kall.pearload.config.Config;
 import me.kall.pearload.config.IConfig;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
-import net.minecraftforge.event.entity.EntityLeaveLevelEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.EntityLeaveLevelEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,10 +32,10 @@ public final class Pearload {
 
     public static final @Nullable IConfig CONFIG = FMLLoader.getLoadingModList().getModFileById("jsonate") == null ? null : new Config();
 
-    public Pearload(@NotNull FMLJavaModLoadingContext context) {
-        context.getModEventBus().addListener(this::setup);
-        MinecraftForge.EVENT_BUS.addListener(this::onEntityLeave);
-        MinecraftForge.EVENT_BUS.addListener(this::onEntityJoin);
+    public Pearload(IEventBus modBus, Dist dist, ModContainer container) {
+        modBus.addListener(this::setup);
+        NeoForge.EVENT_BUS.addListener(this::onEntityLeave);
+        NeoForge.EVENT_BUS.addListener(this::onEntityJoin);
     }
 
     public static boolean projectile() {
@@ -68,7 +70,7 @@ public final class Pearload {
                     modIDs.add(forceLoader);
                 }
             }
-            for (Map.Entry<ResourceKey<EntityType<?>>, EntityType<?>> entry : ForgeRegistries.ENTITY_TYPES.getEntries()) {
+            for (Map.Entry<ResourceKey<EntityType<?>>, EntityType<?>> entry : BuiltInRegistries.ENTITY_TYPE.entrySet()) {
                 ResourceLocation id = entry.getKey().location();
                 EntityType<?> type = entry.getValue();
                 if (registryNames.contains(id) || modIDs.contains(id.getNamespace())) {
