5d06192cb11b8e45165ca85da790294908f7eb17
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-10-07 12:40:07 +0800

    1165 compat

--- src/main/java/me/kall/pearload/data/ForceLoadReasons.java
+++ src/main/java/me/kall/pearload/data/ForceLoadReasons.java
@@ -12,6 +12,7 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraftforge.common.util.Constants;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
@@ -21,6 +22,10 @@ public class ForceLoadReasons extends SavedData {
     private static final String ID = "pearload_force_load_reasons";
 
     private final Object2ObjectMap<ResourceLocation, Long2ObjectMap<Set<UUID>>> data = new Object2ObjectOpenHashMap<>();
+
+    public ForceLoadReasons() {
+        super(ID);
+    }
 
     public boolean add(@NotNull ResourceLocation dimId, long chunkPos, @NotNull UUID uuid) {
         Long2ObjectMap<Set<UUID>> dimMap = data.computeIfAbsent(dimId, k -> new Long2ObjectOpenHashMap<>());
@@ -51,12 +56,12 @@ public class ForceLoadReasons extends SavedData {
     public @NotNull CompoundTag save(@NotNull CompoundTag tag) {
         ListTag dimensions = new ListTag();
 
-        for (var dimEntry : data.object2ObjectEntrySet()) {
+        for (Object2ObjectMap.Entry<ResourceLocation, Long2ObjectMap<Set<UUID>>> dimEntry : data.object2ObjectEntrySet()) {
             CompoundTag dimTag = new CompoundTag();
             dimTag.putString("dim", dimEntry.getKey().toString());
 
             ListTag chunkList = new ListTag();
-            for (var chunkEntry : dimEntry.getValue().long2ObjectEntrySet()) {
+            for (Long2ObjectMap.Entry<Set<UUID>> chunkEntry : dimEntry.getValue().long2ObjectEntrySet()) {
                 CompoundTag chunkTag = new CompoundTag();
                 chunkTag.putLong("pos", chunkEntry.getLongKey());
 
@@ -76,21 +81,21 @@ public class ForceLoadReasons extends SavedData {
         return tag;
     }
 
-    public static @NotNull ForceLoadReasons load(@NotNull CompoundTag tag) {
+    public void load(@NotNull CompoundTag tag) {
         ForceLoadReasons reasons = new ForceLoadReasons();
 
-        ListTag dimensions = tag.getList("dimensions", Tag.TAG_COMPOUND);
+        ListTag dimensions = tag.getList("dimensions", Constants.NBT.TAG_COMPOUND);
         for (Tag dimTag0 : dimensions) {
             CompoundTag dimTag = (CompoundTag) dimTag0;
-            ResourceLocation dimId = ResourceLocation.parse(dimTag.getString("dim"));
+            ResourceLocation dimId = ResourceLocation.tryParse(dimTag.getString("dim"));
             Long2ObjectMap<Set<UUID>> dimMap = new Long2ObjectOpenHashMap<>();
 
-            ListTag chunkList = dimTag.getList("chunks", Tag.TAG_COMPOUND);
+            ListTag chunkList = dimTag.getList("chunks", Constants.NBT.TAG_COMPOUND);
             for (Tag chunkTag0 : chunkList) {
                 CompoundTag chunkTag = (CompoundTag) chunkTag0;
                 long pos = chunkTag.getLong("pos");
                 Set<UUID> uuids = new ObjectOpenHashSet<>();
-                ListTag uuidList = chunkTag.getList("uuids", Tag.TAG_STRING);
+                ListTag uuidList = chunkTag.getList("uuids", Constants.NBT.TAG_STRING);
                 for (Tag uuidTag : uuidList) {
                     uuids.add(UUID.fromString(uuidTag.getAsString()));
                 }
@@ -99,12 +104,10 @@ public class ForceLoadReasons extends SavedData {
 
             reasons.data.put(dimId, dimMap);
         }
-
-        return reasons;
     }
 
 
     public static @NotNull ForceLoadReasons get(@NotNull ServerLevel level) {
-        return level.getDataStorage().computeIfAbsent(ForceLoadReasons::load, ForceLoadReasons::new, ID);
+        return level.getDataStorage().computeIfAbsent(ForceLoadReasons::new, ID);
     }
 }
--- src/main/java/me/kall/pearload/mixin/ServerLevelMixin.java
+++ src/main/java/me/kall/pearload/mixin/ServerLevelMixin.java
@@ -1,10 +1,10 @@
 package me.kall.pearload.mixin;
 
-import me.kall.pearload.api.ForceLoader;
+import me.kall.pearload.Pearload;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,15 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
-    @Shadow public abstract boolean setChunkForced(int chunkX, int chunkZ, boolean add);
-
     @Inject(method = "updateChunkPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunk;removeEntity(Lnet/minecraft/world/entity/Entity;I)V"))
     private void beforeChunkUpdate(Entity entity, CallbackInfo ci) {
-        if (ForceLoader.isForceLoader(entity)) this.setChunkForced(entity.xChunk, entity.zChunk, false);
+        Pearload.handleEntityForceLoadChange(new ChunkPos(entity.xChunk, entity.zChunk), entity.getUUID(), entity, false);
     }
 
     @Inject(method = "updateChunkPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/chunk/LevelChunk;addEntity(Lnet/minecraft/world/entity/Entity;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void afterChunkUpdate(Entity entity, CallbackInfo ci, int chunkX, int y, int chunkZ) {
-        if (ForceLoader.isForceLoader(entity)) this.setChunkForced(chunkX, chunkZ, true);
+        Pearload.handleEntityForceLoadChange(new ChunkPos(chunkX, chunkZ), entity.getUUID(), entity, true);
     }
 }
